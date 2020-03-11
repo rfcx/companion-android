359c3fdffacae3769878b7111de36399c75dabc9
ratree <ratree.ochn@gmail.com>
2020-03-11 14:47:33 +0700

    Set up AudioMoth App project

---
+++ app/src/main/java/org/rfcx/audiomoth/MainActivity.kt
@@ -0,0 +1,15 @@
+package org.rfcx.audiomoth
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
