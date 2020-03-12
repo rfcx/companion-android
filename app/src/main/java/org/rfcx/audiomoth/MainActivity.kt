da2661095b84eb028fb490f01e232b6bd845cc6c
ratree <ratree.ochn@gmail.com>
2020-03-12 11:37:56 +0700

    CA-2077 Can scan QR code

--- app/src/main/java/org/rfcx/audiomoth/MainActivity.kt
+++ app/src/main/java/org/rfcx/audiomoth/MainActivity.kt
@@ -1,13 +1,15 @@
 package org.rfcx.audiomoth
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Paint.UNDERLINE_TEXT_FLAG
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
-import org.rfcx.audiomoth.view.ScanQRCodeActivity
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +19,26 @@ class MainActivity : AppCompatActivity() {
         enterCodeTextView.paintFlags = enterCodeTextView.paintFlags or UNDERLINE_TEXT_FLAG
 
         scanQRButton.setOnClickListener {
-            ScanQRCodeActivity.startActivity(this)
+            IntentIntegrator(this)
+                .setPrompt("Scan a QR code")
+                .setBeepEnabled(false)
+                .initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "The data is empty", Toast.LENGTH_SHORT).show()
+            } else {
+                valueTextView.text = result.contents
+            }
+
+        } else {
+            // the camera will not close if the result is still null
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
