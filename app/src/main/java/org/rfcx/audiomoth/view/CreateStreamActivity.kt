32420d8a564fd7ecad5ebc9cf654fc40a7954667
ratree <ratree.ochn@gmail.com>
2020-03-13 13:45:36 +0700

    CA-2085 Change design for Create Stream page

--- app/src/main/java/org/rfcx/audiomoth/view/CreateStreamActivity.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/CreateStreamActivity.kt
@@ -5,16 +5,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_create_stream.*
 import org.rfcx.audiomoth.R
 
 class CreateStreamActivity : AppCompatActivity() {
 
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    val sites = arrayOf(
+        "Site A",
+        "Site B",
+        "Site C",
+        "Site D",
+        "Site E",
+        "Site F",
+        "Site G",
+        "Site H",
+        "Site I",
+        "Site J"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_stream)
-
 
         if (intent.hasExtra(DEVICE_ID)) {
             val deviceId = intent.getStringExtra(DEVICE_ID)
@@ -22,7 +38,22 @@ class CreateStreamActivity : AppCompatActivity() {
                 deviceIdTextView.text = getString(R.string.device_id_number, deviceId)
             }
         }
-        streamNameTextInput.showKeyboard()
+
+        arrayAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, sites)
+        siteSpinner.adapter = arrayAdapter
+
+        //spinner as dialog
+        siteSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(this@CreateStreamActivity, sites[position], Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+
+        streamNameEditText.showKeyboard()
     }
 
     private fun View.showKeyboard() = this.let {
