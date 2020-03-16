207085fcc69036938a42ead910b8aae660f39046
ratree <ratree.ochn@gmail.com>
2020-03-16 10:19:27 +0700

    CA-2085 Get list of sites

--- app/src/main/java/org/rfcx/audiomoth/view/CreateStreamActivity.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/CreateStreamActivity.kt
@@ -3,34 +3,41 @@ package org.rfcx.audiomoth.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_create_stream.*
 import org.rfcx.audiomoth.R
+import java.util.ArrayList
 
 class CreateStreamActivity : AppCompatActivity() {
 
+    private val db = Firebase.firestore
+
     private var arrayAdapter: ArrayAdapter<String>? = null
-    val sites = arrayOf(
-        "Site A",
-        "Site B",
-        "Site C",
-        "Site D",
-        "Site E",
-        "Site F",
-        "Site G",
-        "Site H",
-        "Site I",
-        "Site J"
-    )
+    val sites: ArrayList<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_stream)
+
+        db.collection("sites")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+//                    Log.d(TAG, "${document.id} => ${document.data}")
+                    sites?.add(document.data["name"].toString())
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
 
         if (intent.hasExtra(DEVICE_ID)) {
             val deviceId = intent.getStringExtra(DEVICE_ID)
@@ -39,13 +46,14 @@ class CreateStreamActivity : AppCompatActivity() {
             }
         }
 
-        arrayAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, sites)
+        arrayAdapter =
+            sites?.let { ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, it) }
         siteSpinner.adapter = arrayAdapter
 
         //spinner as dialog
         siteSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                Toast.makeText(this@CreateStreamActivity, sites[position], Toast.LENGTH_SHORT)
+                Toast.makeText(this@CreateStreamActivity, sites?.get(position), Toast.LENGTH_SHORT)
                     .show()
             }
 
@@ -62,6 +70,7 @@ class CreateStreamActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val TAG = "CreateStreamActivity"
         private const val DEVICE_ID = "DEVICE_ID"
 
         fun startActivity(context: Context, id: String?) {
