381e907f17632a4833bc5d1cb2ca6652383415a2
ratree <ratree.ochn@gmail.com>
2020-03-23 10:11:59 +0700

    CA-2087 Configure - User can add custom recording period (UI)

--- app/src/main/java/org/rfcx/audiomoth/view/configure/ConfigureFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/configure/ConfigureFragment.kt
@@ -1,6 +1,8 @@
 package org.rfcx.audiomoth.view.configure
 
 
+import android.annotation.SuppressLint
+import android.app.TimePickerDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,12 +15,15 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.alert_duration_layout.view.*
 import kotlinx.android.synthetic.main.fragment_configure.*
 import org.rfcx.audiomoth.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ConfigureFragment : Fragment() {
 
     // Todo: save array in string.xml
     private val sampleRateList = arrayOf("8", "16", "32", "48", "96", "192", "256", "384")
     private val gainList = arrayOf("1 - Lowest", "2 - Low", "3 - Medium", "4 - High", "5 - Highest")
+    val calendar = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,13 +33,27 @@ class ConfigureFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_configure, container, false)
     }
 
+    @SuppressLint("SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        addRecordingPeriodTextView.text = getString(R.string.add_recording_period).toUpperCase()
 
         setSampleRateLayout()
         setGainLayout()
         setSleepDurationLayout()
         setRecordingDurationLayout()
+
+        startPeriodTextView.text = SimpleDateFormat("HH:mm").format(calendar.time)
+        endPeriodTextView.text = SimpleDateFormat("HH:mm").format(calendar.time)
+
+        startPeriodLayout.setOnClickListener {
+            setStartPeriod(startPeriodTextView)
+        }
+
+        endPeriodLayout.setOnClickListener {
+            setStartPeriod(endPeriodTextView)
+        }
     }
 
     private fun setSampleRateLayout() {
@@ -124,5 +143,16 @@ class ConfigureFragment : Fragment() {
                     )
                 }
         }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun setStartPeriod(textView: TextView) {
+        val cal = Calendar.getInstance()
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+            textView.text = SimpleDateFormat("HH:mm").format(cal.time)
+        }
+        TimePickerDialog(context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
     }
 }
