1a36bd14ddc9e2977e89b950a5533a93f8f60edb
ratree <ratree.ochn@gmail.com>
2020-05-07 16:00:01 +0700

    CA-2319 Set default pin on map

--- app/src/main/java/org/rfcx/audiomoth/view/configure/DeployFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/configure/DeployFragment.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.mapboxsdk.utils.BitmapUtils
 import org.rfcx.audiomoth.R
 
 class DeployFragment : Fragment(), OnMapReadyCallback {
@@ -40,6 +46,24 @@ class DeployFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(mapboxMap: MapboxMap) {
         this.mapboxMap = mapboxMap
         mapboxMap.setStyle(Style.OUTDOORS) {
+            val symbolManager = SymbolManager(mapView, mapboxMap, it)
+            symbolManager.iconAllowOverlap = true
+            symbolManager.iconIgnorePlacement = true
+
+            val drawable = ResourcesCompat.getDrawable(resources, R.drawable.ic_pin_map, null)
+            val mBitmap = BitmapUtils.getBitmapFromDrawable(drawable)
+            if (mBitmap != null) {
+                it.addImage(PIN_MAP, mBitmap)
+            }
+
+            symbolManager.create(
+                SymbolOptions()
+                    .withLatLng(LatLng(-2.4896794, -46.43152714))
+                    .withIconImage(PIN_MAP)
+                    .withIconSize(1.0f)
+            )
+
+            mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(-2.4896794, -46.43152714), 15.0))
         }
     }
 
@@ -74,6 +98,7 @@ class DeployFragment : Fragment(), OnMapReadyCallback {
     }
 
     companion object {
+        const val PIN_MAP = "pin-map"
         const val MAPBOX_ACCESS_TOKEN =
             "pk.eyJ1IjoicmF0cmVlLW9jaG4iLCJhIjoiY2s5Mjk5MDQ3MDYzcDNmbzVnZHd1aXNqaSJ9.UCrMjgGw8zROm_sRlebSGQ"
     }
