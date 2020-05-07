15999a25fbc65bf95bbb4957f3a439be3c66544c
ratree <ratree.ochn@gmail.com>
2020-05-07 11:48:47 +0700

    CA-2319 Display mapbox (can't show user's location)

--- app/src/main/java/org/rfcx/audiomoth/view/configure/DeployFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/configure/DeployFragment.kt
@@ -1,14 +1,27 @@
 package org.rfcx.audiomoth.view.configure
 
 
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
 import org.rfcx.audiomoth.R
 
-class DeployFragment : Fragment() {
+class DeployFragment : Fragment(), OnMapReadyCallback, PermissionsListener {
+
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var mapView: MapView
+    private var locationManager: LocationManager? = null
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,4 +30,64 @@ class DeployFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_deploy, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        context?.let { Mapbox.getInstance(it, MAPBOX_ACCESS_TOKEN) }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = view.findViewById(R.id.mapBoxView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.OUTDOORS) {
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    companion object {
+        const val MAPBOX_ACCESS_TOKEN =
+            "pk.eyJ1IjoicmF0cmVlLW9jaG4iLCJhIjoiY2s5Mjk5MDQ3MDYzcDNmbzVnZHd1aXNqaSJ9.UCrMjgGw8zROm_sRlebSGQ"
+    }
 }
