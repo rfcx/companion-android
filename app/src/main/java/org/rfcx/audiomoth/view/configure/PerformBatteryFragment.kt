211a21b806a6e2919e23e0ea4c632972a13033b9
ratree <ratree.ochn@gmail.com>
2020-05-19 15:32:01 +0700

    CA-2348 Improve new design UI for Verify page (PerformBatteryFragment)

--- app/src/main/java/org/rfcx/audiomoth/view/configure/PerformBatteryFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/configure/PerformBatteryFragment.kt
@@ -1,28 +1,22 @@
 package org.rfcx.audiomoth.view.configure
 
 import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import kotlinx.android.synthetic.main.fragment_battery_level_bottom_sheet.*
 import kotlinx.android.synthetic.main.fragment_perform_battery.*
 import org.rfcx.audiomoth.R
-import org.rfcx.audiomoth.util.showCommonDialog
-import org.rfcx.audiomoth.util.toDateTimeString
-import java.util.*
+import org.rfcx.audiomoth.view.DeploymentProtocol
 
-class PerformBatteryFragment : Fragment(), BatteryLevelListener {
-
-    private val batteryLevelFragment by lazy { BatteryLevelBottomSheetFragment(this) }
-    lateinit var listener: ConfigureListener
+class PerformBatteryFragment : Fragment() {
+    private var status: String? = null
+    private var deploymentProtocol: DeploymentProtocol? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        listener = (context as ConfigureListener)
+        deploymentProtocol = (context as DeploymentProtocol)
     }
 
     override fun onCreateView(
@@ -30,81 +24,80 @@ class PerformBatteryFragment : Fragment(), BatteryLevelListener {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_perform_battery, container, false)
+        var view = inflater.inflate(R.layout.fragment_perform_battery, container, false)
+        arguments?.let { status = it.getString(STATUS) }
+
+        when (status) {
+            TIME_LED_FLASH -> view =
+                inflater.inflate(R.layout.fragment_battery_level_bottom_sheet, container, false)
+            BATTERY_LEVEL -> view =
+                inflater.inflate(R.layout.confirm_perform_battery, container, false)
+        }
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        checkBatteryButton.setOnClickListener {
-            activity?.showCommonDialog(null,
-                getString(
-                    R.string.msg_ask_count_flashes
-                ), DialogInterface.OnClickListener { dialog, _ ->
-                    batteryLevelFragment.show(
-                        childFragmentManager,
-                        BatteryLevelBottomSheetFragment.TAG
-                    )
-                    dialog.dismiss()
+        when (status) {
+            TEST_BATTERY -> {
+                testButton.setOnClickListener {
+                    deploymentProtocol?.openPerformBattery(TIME_LED_FLASH)
                 }
-            )
-        }
 
-        skipButton.setOnClickListener {
-            predictBatteryLife()
-        }
-    }
-
-    override fun onSelectedBatteryLevel(level: Int) {
-        batteryLevelFragment.dismiss()
-        predictBatteryLife(level)
-    }
-
-    private fun predictBatteryLife(batteryLv: Int = 3) {
-        // TODO: Update later it is mockup!
-        val today = Calendar.getInstance()
-        val mockPredict = Date(today.timeInMillis + (batteryLv * 24 * 60 * 60 * 1000))
-        activity?.showCommonDialog(null,
-            getString(
-                R.string.format_confirm_perform_battery,
-                mockPredict.toDateTimeString()
-            ), DialogInterface.OnClickListener { dialog, _ ->
-                listener.openDeploy(batteryLv, mockPredict.time)
-                dialog.dismiss()
+                skipButton.setOnClickListener {
+                    deploymentProtocol?.nextStep()
+                }
             }
-        )
+            TIME_LED_FLASH -> {
+
+            }
+            BATTERY_LEVEL -> {
+
+            }
+        }
     }
 
     companion object {
         const val TAG = "PerformBatteryFragment"
+        const val STATUS = "STATUS"
+        const val TEST_BATTERY = "TEST_BATTERY"
+        const val TIME_LED_FLASH = "TIME_LED_FLASH"
+        const val BATTERY_LEVEL = "BATTERY_LEVEL"
+
+        @JvmStatic
+        fun newInstance(page: String) = PerformBatteryFragment().apply {
+            arguments = Bundle().apply {
+                putString(STATUS, page)
+            }
+        }
     }
 }
 
-interface BatteryLevelListener {
-    fun onSelectedBatteryLevel(level: Int)
-}
-
-class BatteryLevelBottomSheetFragment(private val listener: BatteryLevelListener) :
-    BottomSheetDialogFragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_battery_level_bottom_sheet, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setView()
-    }
-
-    private fun setView() {
-        batteryLv1Button.setOnClickListener { listener.onSelectedBatteryLevel(1) }
-        batteryLv2Button.setOnClickListener { listener.onSelectedBatteryLevel(2) }
-        batteryLv3Button.setOnClickListener { listener.onSelectedBatteryLevel(3) }
-    }
-
-    companion object {
-        const val TAG = "BatteryLevelDialog"
-    }
-}
+//interface BatteryLevelListener {
+//    fun onSelectedBatteryLevel(level: Int)
+//}
+//
+//class BatteryLevelBottomSheetFragment(private val listener: BatteryLevelListener) :
+//    BottomSheetDialogFragment() {
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.fragment_battery_level_bottom_sheet, container, false)
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        setView()
+//    }
+//
+//    private fun setView() {
+//        batteryLv1Button.setOnClickListener { listener.onSelectedBatteryLevel(1) }
+//        batteryLv2Button.setOnClickListener { listener.onSelectedBatteryLevel(2) }
+//        batteryLv3Button.setOnClickListener { listener.onSelectedBatteryLevel(3) }
+//    }
+//
+//    companion object {
+//        const val TAG = "BatteryLevelDialog"
+//    }
+//}
