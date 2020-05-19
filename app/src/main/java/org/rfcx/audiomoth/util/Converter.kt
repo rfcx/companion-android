b7dd61d2079d74faed9e64baf4167d61cf674ee6
Anuphap Suwannamas <anupharpae@gmail.com>
2020-05-19 13:54:07 +0700

    CA-2338 user can press pin and show pin info

---
+++ app/src/main/java/org/rfcx/audiomoth/util/Converter.kt
@@ -0,0 +1,11 @@
+package org.rfcx.audiomoth.util
+
+import com.google.gson.JsonObject
+
+fun Map<String, String>.toJsonObject(): JsonObject {
+    val jsonObject = JsonObject()
+    this.forEach {
+        jsonObject.addProperty(it.key, it.value)
+    }
+    return jsonObject
+}
