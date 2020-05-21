4073dee92c0bda346e313e869bf77babae19074b
ratree <ratree.ochn@gmail.com>
2020-05-21 14:02:17 +0700

    CA-2351 User can login with email and password

--- app/src/main/java/org/rfcx/audiomoth/view/LoginActivity.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/LoginActivity.kt
@@ -5,10 +5,27 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.BaseCallback
+import com.auth0.android.result.Credentials
 import kotlinx.android.synthetic.main.activity_login.*
 import org.rfcx.audiomoth.R
 
 class LoginActivity : AppCompatActivity() {
+
+    private val auth0 by lazy {
+        val auth0 =
+            Auth0(this.getString(R.string.auth0_client_id), this.getString(R.string.auth0_domain))
+        auth0.isOIDCConformant = true
+        auth0.isLoggingEnabled = true
+        auth0
+    }
+
+    private val authentication by lazy {
+        AuthenticationAPIClient(auth0)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +38,24 @@ class LoginActivity : AppCompatActivity() {
 
             if (validateInput(email, password)) {
                 loading()
+                login(email, password)
             }
         }
+    }
+
+    private fun login(email: String, password: String) {
+        authentication.login(email, password, "Username-Password-Authentication")
+            .setScope(this.getString(R.string.auth0_scopes))
+            .setAudience(this.getString(R.string.auth0_audience))
+            .start(object : BaseCallback<Credentials, AuthenticationException> {
+                override fun onSuccess(credentials: Credentials) {
+
+                }
+
+                override fun onFailure(exception: AuthenticationException) {
+                    exception.printStackTrace()
+                }
+            })
     }
 
     private fun loading(start: Boolean = true) {
@@ -42,7 +75,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun View.hideKeyboard() = this.let {
-        val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }
