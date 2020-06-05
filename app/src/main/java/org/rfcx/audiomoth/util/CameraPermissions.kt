feebe69987b15254839ad686a6c8cc5d44933224
ratree <ratree.ochn@gmail.com>
2020-06-05 15:33:28 +0700

    CA-2412 Change position of feedback button

--- app/src/main/java/org/rfcx/audiomoth/util/CameraPermissions.kt
+++ app/src/main/java/org/rfcx/audiomoth/util/CameraPermissions.kt
@@ -36,37 +36,6 @@ class CameraPermissions(private val activity: Activity) {
         }
     }
 
-    fun handleRequestResult(requestCode: Int, grantResults: IntArray) {
-        if (requestCode == REQUEST_PERMISSIONS_IMAGE_CAPTURE) {
-            if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED ||
-                grantResults[1] != PackageManager.PERMISSION_GRANTED) {
-                val shouldProvideCamera = ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                    Manifest.permission.CAMERA)
-                val shouldProvideStorage = ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                if (!shouldProvideCamera || !shouldProvideStorage) {
-                    val dialogBuilder: AlertDialog.Builder =
-                        AlertDialog.Builder(activity).apply {
-                            setTitle(null)
-                            setMessage(R.string.camera_permission_msg)
-                            setPositiveButton(R.string.go_to_setting) { _, _ ->
-                                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                                    Uri.parse("package:${activity.packageName}"))
-                                intent.addCategory(Intent.CATEGORY_DEFAULT)
-                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                                activity.startActivity(intent)
-                            }
-                        }
-                    dialogBuilder.create().show()
-                } else {
-                    onCompletionCallback?.invoke(false)
-                }
-            } else {
-                onCompletionCallback?.invoke(true)
-            }
-        }
-    }
-
     companion object {
         const val REQUEST_PERMISSIONS_IMAGE_CAPTURE = 4000
     }
