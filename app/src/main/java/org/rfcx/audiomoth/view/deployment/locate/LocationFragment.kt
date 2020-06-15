5b174330da22224cdf3789e3fa0b9443dc60d8ac
ratree <ratree.ochn@gmail.com>
2020-06-15 12:52:43 +0700

    MA-2435 Fix save locate to firestore

--- app/src/main/java/org/rfcx/audiomoth/view/deployment/locate/LocationFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/deployment/locate/LocationFragment.kt
@@ -34,6 +34,7 @@ import kotlinx.android.synthetic.main.fragment_location.*
 import org.rfcx.audiomoth.R
 import org.rfcx.audiomoth.entity.Locate
 import org.rfcx.audiomoth.localdb.LocateDb
+import org.rfcx.audiomoth.util.Firestore
 import org.rfcx.audiomoth.util.RealmHelper
 import org.rfcx.audiomoth.view.deployment.DeploymentProtocol
 
@@ -115,6 +116,7 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
         val lat = latitudeEditText.text.toString().toDouble()
         val lng = longitudeEditText.text.toString().toDouble()
         val locate = Locate(name = name, latitude = lat, longitude = lng)
+        context?.let { Firestore(it).saveLocate(locateDb, locate) }
         deploymentProtocol?.setDeployLocation(locate)
         deploymentProtocol?.nextStep()
     }
