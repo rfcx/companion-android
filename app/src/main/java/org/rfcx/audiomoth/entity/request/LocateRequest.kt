6c7c5b21b3965a23078bdfee107903e67073df8f
anuphap <anupharpae@gmail.com>
2020-06-21 11:13:11 +0700

    Adjust locate request body to add more last deployment server id and fixed send deployment to firestore by use request body model

--- app/src/main/java/org/rfcx/audiomoth/entity/request/LocateRequest.kt
+++ app/src/main/java/org/rfcx/audiomoth/entity/request/LocateRequest.kt
@@ -8,7 +8,8 @@ data class LocateRequest(
     var latitude: Double = 0.0,
     var longitude: Double = 0.0,
     var createdAt: Date = Date(),
-    var lastDeployment: Int = 0
+    var lastDeployment: Int = 0,
+    var lastDeploymentServerId: String? = null
 )
 
 fun Locate.toRequestBody(): LocateRequest {
