56050d7e0e03f9ae5c0112da7e5fc24904d33ccf
wasuphon <wasuphonn59@email.nu.ac.th>
2020-06-25 16:19:22 +0700

    Add firestore function for guardian sync worker

---
+++ app/src/main/java/org/rfcx/audiomoth/entity/request/GuardianProfileRequest.kt
@@ -0,0 +1,26 @@
+package org.rfcx.audiomoth.entity.request
+
+import org.rfcx.audiomoth.entity.guardian.GuardianProfile
+import java.util.*
+
+data class GuardianProfileRequest(
+    var device: String,
+    var name: String = "",
+    var sampleRate: Int = 0,
+    var bitrate: Int = 0,
+    var fileFormat: String = "",
+    var duration: Int = 0,
+    var createdAt: Date = Date()
+)
+
+fun GuardianProfile.toRequestBody(): GuardianProfileRequest {
+    return GuardianProfileRequest(
+        device = "guardian",
+        name = this.name,
+        sampleRate = this.sampleRate,
+        bitrate = this.bitrate,
+        fileFormat = this.fileFormat,
+        duration = this.duration,
+        createdAt = this.createdAt
+    )
+}
