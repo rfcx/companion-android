86de52612238d394465f844bcea213b2aec5a3ee
ratree <ratree.ochn@gmail.com>
2020-07-11 22:30:01 +0700

    MA-2521 Improve time line

--- app/src/main/java/org/rfcx/audiomoth/view/detail/TimeLineAdapter.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/detail/TimeLineAdapter.kt
@@ -5,10 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_timeline.view.*
 import org.rfcx.audiomoth.R
+import org.rfcx.audiomoth.util.getIntColor
 
 
 class TimeLineAdapter : RecyclerView.Adapter<TimeLineAdapter.TimeLineViewHolder>() {
-
     var items: ArrayList<String> = arrayListOf()
         set(value) {
             field = value
@@ -26,20 +26,37 @@ class TimeLineAdapter : RecyclerView.Adapter<TimeLineAdapter.TimeLineViewHolder>
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: TimeLineViewHolder, position: Int) {
-        holder.bind(items[position])
+        holder.bind(items[position], items)
     }
 
     class TimeLineViewHolder(itemView: View, viewType: Int) :
         RecyclerView.ViewHolder(itemView) {
         private val customRecording = itemView.timelineTextView
         private val timeline = itemView.timeline
+        private val viewTypeViewHolder = viewType
+        private val context = itemView.context
 
-        fun bind(item: String) {
+        fun bind(item: String, items: ArrayList<String>) {
             customRecording.text = item
-        }
+            timeline.initLine(viewTypeViewHolder)
 
-        init {
-            timeline.initLine(viewType)
+            if (items[0] == item) {
+                timeline.setStartLineColor(context.getIntColor(R.color.white), viewTypeViewHolder)
+            } else {
+                timeline.setStartLineColor(
+                    context.getIntColor(R.color.colorPrimary),
+                    viewTypeViewHolder
+                )
+            }
+
+            if (items.last() == item) {
+                timeline.setEndLineColor(context.getIntColor(R.color.white), viewTypeViewHolder)
+            } else {
+                timeline.setEndLineColor(
+                    context.getIntColor(R.color.colorPrimary),
+                    viewTypeViewHolder
+                )
+            }
         }
     }
 }
