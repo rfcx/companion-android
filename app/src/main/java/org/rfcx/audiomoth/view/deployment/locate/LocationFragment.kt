c2a380541f911cce20f998c5cc260c6daa733e71
anuphap <anupharpae@gmail.com>
2020-07-13 21:55:17 +0700

    MA-2501 Fixed follow comment from PR

--- app/src/main/java/org/rfcx/audiomoth/view/deployment/locate/LocationFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/deployment/locate/LocationFragment.kt
@@ -44,10 +44,7 @@ import kotlinx.android.synthetic.main.fragment_location.*
 import org.rfcx.audiomoth.R
 import org.rfcx.audiomoth.entity.Locate
 import org.rfcx.audiomoth.localdb.LocateDb
-import org.rfcx.audiomoth.util.LocationPermissions
-import org.rfcx.audiomoth.util.RealmHelper
-import org.rfcx.audiomoth.util.latitudeCoordinates
-import org.rfcx.audiomoth.util.longitudeCoordinates
+import org.rfcx.audiomoth.util.*
 import org.rfcx.audiomoth.view.deployment.BaseDeploymentProtocal
 
 class LocationFragment : Fragment(), OnMapReadyCallback {
@@ -358,13 +355,14 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
 
     private fun setupInputLocation() {
         latitudeEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-                if (p0 != null && p0.isNotBlank()) {
-                    if (newLocationRadioButton.isChecked) {
-                        if (p0[0] != '.' && p0.last() != '.' && !(p0[0] == '-' && p0.length == 1) && p0.last() != '-') {
-                            convertInputLatitude(p0.toString())
-                        }
-                    }
+            override fun afterTextChanged(latitudeInput: Editable?) {
+                val longitudeInput = longitudeEditText.text?.trim()
+
+                if (newLocationRadioButton.isChecked &&
+                    latitudeInput != null && latitudeInput.toString().isCoordinates() &&
+                    longitudeInput != null && longitudeInput.toString().isCoordinates()
+                ) {
+                    convertInputLatitude(latitudeInput.toString(), longitudeInput.toString())
                 }
             }
 
@@ -374,13 +372,13 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
         })
 
         longitudeEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-                if (p0 != null && p0.isNotBlank()) {
-                    if (newLocationRadioButton.isChecked) {
-                        if (p0[0] != '.' && p0.last() != '.' && !(p0[0] == '-' && p0.length == 1) && p0.last() != '-') {
-                            convertInputLongitude(p0.toString())
-                        }
-                    }
+            override fun afterTextChanged(input: Editable?) {
+                val latitudeInput = latitudeEditText.text?.trim()
+                if (newLocationRadioButton.isChecked &&
+                    input != null && input.toString().isCoordinates()
+                    && latitudeInput != null && latitudeInput.toString().isCoordinates()
+                ) {
+                    convertInputLongitude(input.toString())
                 }
             }
 
@@ -390,17 +388,13 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
         })
     }
 
-    fun convertInputLatitude(latitude: String) {
-        val longitude = longitudeEditText.text?.trim()
-        if (longitude == null || longitude.isBlank() || longitude.last() == '-' || longitude.last() == '.') return
-
+    fun convertInputLatitude(latitude: String, longitude: String) {
         val lat = latitude.toDouble()
-        val long = longitude.toString()
 
-        if (long.last() != 'E' && long.last() != 'W') {
+        if (longitude.last() != 'E' && longitude.last() != 'W') {
             if (lat < 90 && lat > -90) {
                 setPinOnMap(
-                    LatLng(lat, longitudeEditText.text.toString().toDouble())
+                    LatLng(lat, longitude.toDouble())
                 )
             } else {
                 Toast.makeText(
@@ -413,9 +407,6 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
     }
 
     fun convertInputLongitude(longitude: String) {
-        val latitude = latitudeEditText.text?.trim()
-        if (latitude == null || latitude.isBlank() || latitude.last() == '-' || latitude.last() == '.') return
-
         val long = longitude.toDouble()
         if (long < 180 && long > -180) {
             setPinOnMap(
@@ -452,7 +443,7 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
         return permissionState == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun requestPermissions() {
+    private fun hasPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             activity?.requestPermissions(
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -486,7 +477,7 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
 
             initLocationEngine()
         } else {
-            requestPermissions()
+            hasPermission()
         }
     }
 
