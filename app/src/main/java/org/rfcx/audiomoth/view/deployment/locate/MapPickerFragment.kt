1ab1ade452aa33f2f72e415863dc71149f398e5a
ratree <ratree.ochn@gmail.com>
2020-07-15 21:31:21 +0700

    MA-2554 When change map update location text

--- app/src/main/java/org/rfcx/audiomoth/view/deployment/locate/MapPickerFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/deployment/locate/MapPickerFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
@@ -12,7 +13,10 @@ import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import kotlinx.android.synthetic.main.fragment_map_picker.*
 import org.rfcx.audiomoth.R
+import org.rfcx.audiomoth.util.latitudeCoordinates
+import org.rfcx.audiomoth.util.longitudeCoordinates
 
 class MapPickerFragment : Fragment(), OnMapReadyCallback {
     private var mapboxMap: MapboxMap? = null
@@ -35,14 +39,33 @@ class MapPickerFragment : Fragment(), OnMapReadyCallback {
         mapView = view.findViewById(R.id.mapBoxPickerView)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
+
+        selectButton.setOnClickListener {
+            Toast.makeText(context, "SETECT", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onMapReady(mapboxMap: MapboxMap) {
         this.mapboxMap = mapboxMap
         mapboxMap.uiSettings.isAttributionEnabled = false
         mapboxMap.uiSettings.isLogoEnabled = false
-        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(16.0,100.0), 15.0))
+        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(16.0, 100.0), 15.0))
         mapboxMap.setStyle(Style.OUTDOORS)
+
+        mapboxMap.addOnCameraMoveListener {
+            val currentCameraPosition = mapboxMap.cameraPosition.target
+            setLatLogLabel(LatLng(currentCameraPosition.latitude, currentCameraPosition.longitude))
+        }
+    }
+
+    private fun setLatLogLabel(location: LatLng) {
+        context?.let {
+            val latLng =
+                "${location.latitude.latitudeCoordinates(it)}, ${location.longitude.longitudeCoordinates(
+                    it
+                )}"
+            locationTextView.text = latLng
+        }
     }
 
     override fun onStart() {
