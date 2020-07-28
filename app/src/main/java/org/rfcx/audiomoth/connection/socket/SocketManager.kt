f295115c027145c2268769f1b1f6b322abfca1b4
wasuphon <wasuphonn59@email.nu.ac.th>
2020-07-28 12:27:36 +0700

    Remove imports

--- app/src/main/java/org/rfcx/audiomoth/connection/socket/SocketManager.kt
+++ app/src/main/java/org/rfcx/audiomoth/connection/socket/SocketManager.kt
@@ -8,8 +8,6 @@ import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.net.ConnectException
 import java.net.Socket
-import org.json.JSONObject
-import org.rfcx.audiomoth.entity.socket.*
 
 object SocketManager {
 
