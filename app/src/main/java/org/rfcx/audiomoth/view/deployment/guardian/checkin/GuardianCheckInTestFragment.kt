43de267cff02557f6cc74f72230526ae85f20899
wasuphon <wasuphonn59@email.nu.ac.th>
2020-08-19 19:36:53 +0700

    Fix wrong id

--- app/src/main/java/org/rfcx/audiomoth/view/deployment/guardian/checkin/GuardianCheckInTestFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/deployment/guardian/checkin/GuardianCheckInTestFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_guardian_signal.*
+import kotlinx.android.synthetic.main.fragment_guardian_checkin_test.*
 import org.rfcx.audiomoth.R
 import org.rfcx.audiomoth.view.deployment.guardian.GuardianDeploymentProtocol
 
@@ -30,7 +30,7 @@ class GuardianCheckInTestFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        finishButton.setOnClickListener {
+        checkInFinishButton.setOnClickListener {
             deploymentProtocol?.nextStep()
         }
     }
