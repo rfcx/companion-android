9d5d1090bb7466e880d373d7f7b98060cb6faff4
wasuphon <wasuphonn59@email.nu.ac.th>
2020-08-28 20:38:26 +0700

    Hide finish button

--- app/src/main/java/org/rfcx/audiomoth/view/deployment/guardian/solarpanel/GuardianSolarPanelFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/deployment/guardian/solarpanel/GuardianSolarPanelFragment.kt
@@ -39,6 +39,8 @@ class GuardianSolarPanelFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        deploymentProtocol?.hideCompleteButton()
+
         setVoltageValue()
         setCurrentValue()
         setPowerValue()
