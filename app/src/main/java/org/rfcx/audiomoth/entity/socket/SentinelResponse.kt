9da5ec5cdee871da5567250007e4f15e77089554
wasuphon <wasuphonn59@email.nu.ac.th>
2020-08-30 19:25:47 +0700

    Add serialize for isSolarAttached

--- app/src/main/java/org/rfcx/audiomoth/entity/socket/SentinelResponse.kt
+++ app/src/main/java/org/rfcx/audiomoth/entity/socket/SentinelResponse.kt
@@ -1,10 +1,13 @@
 package org.rfcx.audiomoth.entity.socket
 
+import com.google.gson.annotations.SerializedName
+
 data class SentinelResponse(
     val sentinel: SentinelInfo = SentinelInfo()
 ) : SocketResposne
 
 data class SentinelInfo(
+    @SerializedName("is_solar_attached")
     val isSolarAttached: Boolean = false,
     val voltage: Int = 0,
     val current: Int = 0,
