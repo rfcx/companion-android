a49a94db371eb2d290bb16a9fb357c52a516b142
ratree <ratree.ochn@gmail.com>
2020-10-06 21:22:04 +0700

    MA-2641 Change to keep in array file

--- app/src/main/java/org/rfcx/audiomoth/view/profile/locationgroup/CreateNewGroupActivity.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/profile/locationgroup/CreateNewGroupActivity.kt
@@ -24,28 +24,6 @@ class CreateNewGroupActivity : AppCompatActivity(), (ColorPickerItem, Int) -> Un
     private val locationGroupDb by lazy { LocationGroupDb(realm) }
     private val colorPickerAdapter by lazy { ColorPickerAdapter(this) }
     private var colorPickerState = ArrayList<ColorPickerItem>()
-    private val colorPickerList = arrayListOf(
-        "#ff7575",
-        "#ffcd00",
-        "#ba4d4d",
-        "#31984f",
-        "#9d9d9d",
-        "#f6402c",
-        "#eb1460",
-        "#9c1ab1",
-        "#6633b9",
-        "#3d4db7",
-        "#46af4a",
-        "#129788",
-        "#8cfffb",
-        "#00bbd5",
-        "#00a6f6",
-        "#88c440",
-        "#ccdd1e",
-        "#ff9800",
-        "#7a5547",
-        "#5e7c8b"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,6 +36,7 @@ class CreateNewGroupActivity : AppCompatActivity(), (ColorPickerItem, Int) -> Un
             adapter = colorPickerAdapter
         }
 
+        val colorPickerList = this.resources.getStringArray(R.array.edge_color_picker).toList()
         colorPickerList.forEach {
             colorPickerState.add(ColorPickerItem(it, colorPickerList.first() == it))
         }
@@ -79,6 +58,7 @@ class CreateNewGroupActivity : AppCompatActivity(), (ColorPickerItem, Int) -> Un
         }
     }
     override fun invoke(colorPickerItem: ColorPickerItem, position: Int) {
+        val colorPickerList = this.resources.getStringArray(R.array.edge_color_picker).toList()
         colorPickerList.forEachIndexed { index, _ ->
             colorPickerState[index] = ColorPickerItem(colorPickerList[index], position == index)
         }
