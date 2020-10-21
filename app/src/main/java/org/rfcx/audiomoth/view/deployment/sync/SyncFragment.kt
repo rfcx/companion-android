e1847b30b1f1e98e547da5653af8aa973135d822
ratree <ratree.ochn@gmail.com>
2020-10-21 18:48:04 +0700

    MA-2769 Add sync tone page and after sync tone

--- app/src/main/java/org/rfcx/audiomoth/view/deployment/sync/SyncFragment.kt
+++ app/src/main/java/org/rfcx/audiomoth/view/deployment/sync/SyncFragment.kt
@@ -7,8 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.fragment_after_sync.*
+import kotlinx.android.synthetic.main.fragment_after_play_initial_tone.*
+import kotlinx.android.synthetic.main.fragment_after_play_initial_tone.noButton
+import kotlinx.android.synthetic.main.fragment_after_play_initial_tone.yesButton
+import kotlinx.android.synthetic.main.fragment_after_play_sync_tone.*
 import kotlinx.android.synthetic.main.fragment_initial_tone_playing.*
+import kotlinx.android.synthetic.main.fragment_play_sync_tone.*
 import kotlinx.android.synthetic.main.fragment_start_sync_process.*
 import org.rfcx.audiomoth.R
 import org.rfcx.audiomoth.view.deployment.EdgeDeploymentProtocol
@@ -22,12 +26,6 @@ class SyncFragment : Fragment() {
         edgeDeploymentProtocol = (context as EdgeDeploymentProtocol)
     }
 
-//    start_sync_process
-//    initial_tone_playing_process
-//    after_play_initial_tone_process
-//    play_sync_tone
-//    after_play_sync_tone
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,7 +37,12 @@ class SyncFragment : Fragment() {
         when (status) {
             INITIAL_TONE_PLAYING -> view =
                 inflater.inflate(R.layout.fragment_initial_tone_playing, container, false)
-            AFTER_PLAY_INITIAL_TONE -> view = inflater.inflate(R.layout.fragment_after_sync, container, false)
+            AFTER_PLAY_INITIAL_TONE -> view =
+                inflater.inflate(R.layout.fragment_after_play_initial_tone, container, false)
+            PLAY_SYNC_TONE -> view =
+                inflater.inflate(R.layout.fragment_play_sync_tone, container, false)
+            AFTER_PLAY_SYNC_TONE -> view =
+                inflater.inflate(R.layout.fragment_after_play_sync_tone, container, false)
         }
         return view
     }
@@ -56,18 +59,13 @@ class SyncFragment : Fragment() {
         when (status) {
             START_SYNC -> startSyncProcess()
             INITIAL_TONE_PLAYING -> initialTonePlaying()
-            AFTER_PLAY_INITIAL_TONE -> afterSync()
+            AFTER_PLAY_INITIAL_TONE -> afterPlayInitialTone()
+            PLAY_SYNC_TONE -> playSyncTone()
+            AFTER_PLAY_SYNC_TONE -> afterPlaySyncTone()
         }
     }
 
     private fun startSyncProcess() {
-//        nextButton.isSoundEffectsEnabled = false
-//
-//        nextButton.setOnClickListener {
-//            edgeDeploymentProtocol?.playSyncSound()
-//            edgeDeploymentProtocol?.startSyncing(SYNCING)
-//        }
-
         playToneButton.setOnClickListener {
             edgeDeploymentProtocol?.playTone()
         }
@@ -80,13 +78,36 @@ class SyncFragment : Fragment() {
         }
     }
 
-    private fun afterSync() {
+    private fun afterPlayInitialTone() {
+        Glide.with(this).load(R.drawable.audiomoth_flashing_green)
+            .into(audioMothFlashingGreenImageView)
+
+        yesButton.setOnClickListener {
+            edgeDeploymentProtocol?.startSyncing(PLAY_SYNC_TONE)
+        }
+
+        noButton.setOnClickListener {
+            edgeDeploymentProtocol?.startSyncing(START_SYNC)
+        }
+    }
+
+    private fun playSyncTone() {
+        playSyncToneButton.isSoundEffectsEnabled = false
+        playSyncToneButton.setOnClickListener {
+            edgeDeploymentProtocol?.playSyncSound()
+            edgeDeploymentProtocol?.startSyncing(AFTER_PLAY_SYNC_TONE)
+        }
+    }
+
+    private fun afterPlaySyncTone() {
+        Glide.with(this).load(R.drawable.audiomoth_flashing_red)
+            .into(audioMothFlashingRedImageView)
         yesButton.setOnClickListener {
             edgeDeploymentProtocol?.nextStep()
         }
 
         noButton.setOnClickListener {
-//            edgeDeploymentProtocol?.startSyncing(BEFORE_SYNC)
+            edgeDeploymentProtocol?.startSyncing(START_SYNC)
         }
     }
 
