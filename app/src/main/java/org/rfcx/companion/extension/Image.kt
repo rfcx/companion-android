0bb5b716c5c01d8941041a10ec3d91c602ab822d
ratreeochn <ratree.ochn@gmail.com>
2021-05-18 16:46:24 +0700

    CE-680 White box while the photo make it grey and show loading animation

--- app/src/main/java/org/rfcx/companion/extension/Image.kt
+++ app/src/main/java/org/rfcx/companion/extension/Image.kt
@@ -1,14 +1,23 @@
 package org.rfcx.companion.extension
 
+import android.graphics.drawable.Drawable
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.LazyHeaders
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import jp.wasabeef.glide.transformations.BlurTransformation
 import org.rfcx.companion.R
 
-fun ImageView.setDeploymentImage(url: String, blur: Boolean, fromServer: Boolean, token: String? = null) {
+fun ImageView.setDeploymentImage(url: String, blur: Boolean, fromServer: Boolean, token: String? = null, progressBar: ProgressBar) {
+    progressBar.visibility = View.VISIBLE
+
     if (fromServer) {
         val glideUrl = GlideUrl(
             url,
@@ -19,6 +28,28 @@ fun ImageView.setDeploymentImage(url: String, blur: Boolean, fromServer: Boolean
 
         Glide.with(this)
             .load(glideUrl)
+            .listener(object : RequestListener<Drawable>{
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    return false
+                }
+            })
             .placeholder(R.drawable.bg_grey_light)
             .error(R.drawable.bg_grey_light)
             .into(this)
