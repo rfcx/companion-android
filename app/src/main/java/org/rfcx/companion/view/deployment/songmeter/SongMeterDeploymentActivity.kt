f854b33d0fdfc75307690cb918b8fed93df7b8c1
ratreeochn <ratree.ochn@gmail.com>
2021-06-29 11:02:43 +0700

    CE-1012 Add checklist page

--- app/src/main/java/org/rfcx/companion/view/deployment/songmeter/SongMeterDeploymentActivity.kt
+++ app/src/main/java/org/rfcx/companion/view/deployment/songmeter/SongMeterDeploymentActivity.kt
@@ -2,13 +2,20 @@ package org.rfcx.companion.view.deployment.songmeter
 
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_song_meter_deployment.*
 import kotlinx.android.synthetic.main.toolbar_default.*
 import org.rfcx.companion.R
-import org.rfcx.companion.view.deployment.EdgeDeploymentActivity
+import org.rfcx.companion.entity.DeploymentLocation
+import org.rfcx.companion.entity.Locate
+import org.rfcx.companion.entity.Project
+import org.rfcx.companion.service.DownloadStreamState
+import org.rfcx.companion.view.deployment.locate.SiteWithLastDeploymentItem
 
-class SongMeterDeploymentActivity : AppCompatActivity() {
+class SongMeterDeploymentActivity : AppCompatActivity(), SongMeterDeploymentProtocol {
 
     companion object {
         const val TAG = "SongMeterDeploymentActivity"
@@ -23,6 +30,109 @@ class SongMeterDeploymentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_song_meter_deployment)
 
         setupToolbar()
+        startCheckList()
+    }
+
+    override fun startMapPicker(latitude: Double, longitude: Double, siteId: Int, name: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun startCheckList() {
+        startFragment(SongMeterCheckListFragment.newInstance())
+    }
+
+    override fun startDetailDeploymentSite(id: Int, name: String?, isNewSite: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isOpenedFromUnfinishedDeployment(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun nextStep() {
+        TODO("Not yet implemented")
+    }
+
+    override fun backStep() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDeploymentLocation(): DeploymentLocation? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSiteItem(): ArrayList<SiteWithLastDeploymentItem> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLocationGroup(name: String): Project? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getImages(): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCurrentLocation(): Location {
+        TODO("Not yet implemented")
+    }
+
+    override fun setDeployLocation(locate: Locate, isExisted: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setSiteItem(items: ArrayList<SiteWithLastDeploymentItem>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setImages(images: List<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setReadyToDeploy() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCurrentLocation(location: Location) {
+        TODO("Not yet implemented")
+    }
+
+    override fun handleCheckClicked(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPassedChecks(): List<Int> {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCurrentPage(name: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showToolbar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideToolbar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setToolbarTitle() {
+        TODO("Not yet implemented")
+    }
+
+    override fun isSiteLoading(): DownloadStreamState {
+        TODO("Not yet implemented")
+    }
+
+    override fun showSiteLoadingDialog(text: String) {
+        TODO("Not yet implemented")
+    }
+
+    private fun startFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(contentContainer.id, fragment)
+            .commit()
     }
 
     private fun setupToolbar() {
---
+++ app/src/main/java/org/rfcx/companion/view/deployment/songmeter/SongMeterDeploymentProtocol.kt
@@ -0,0 +1,5 @@
+package org.rfcx.companion.view.deployment.songmeter
+
+import org.rfcx.companion.view.deployment.BaseDeploymentProtocol
+
+interface SongMeterDeploymentProtocol : BaseDeploymentProtocol
