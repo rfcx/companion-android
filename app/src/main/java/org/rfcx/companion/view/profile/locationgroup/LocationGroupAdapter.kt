5e50af9da8cd1abbfa04d278a75c3ebd1bcc9adc
RatreeOchn <ratree.ochn@gmail.com>
2021-07-29 23:47:01 +0700

    CE-1182 Set role lable on Map and set deployment site page

--- app/src/main/java/org/rfcx/companion/view/profile/locationgroup/LocationGroupAdapter.kt
+++ app/src/main/java/org/rfcx/companion/view/profile/locationgroup/LocationGroupAdapter.kt
@@ -3,11 +3,13 @@ package org.rfcx.companion.view.profile.locationgroup
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_location_group.view.*
 import org.rfcx.companion.R
 import org.rfcx.companion.entity.Project
 import org.rfcx.companion.entity.Screen
+import org.rfcx.companion.entity.isGuest
 
 class LocationGroupAdapter(private val locationGroupListener: LocationGroupListener) :
     RecyclerView.Adapter<LocationGroupAdapter.LocationGroupAdapterViewHolder>() {
@@ -31,7 +33,7 @@ class LocationGroupAdapter(private val locationGroupListener: LocationGroupListe
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: LocationGroupAdapterViewHolder, position: Int) {
-        holder.bind(items[position].name ?: holder.itemView.context.getString(R.string.none))
+        holder.bind(items[position])
         holder.itemView.setOnClickListener {
             locationGroupListener.onClicked(items[position])
         }
@@ -40,14 +42,41 @@ class LocationGroupAdapter(private val locationGroupListener: LocationGroupListe
     inner class LocationGroupAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val locationGroupTextView = itemView.locationGroupTextView
         private val checkImageView = itemView.checkImageView
+        private val permissionsTextView = itemView.permissionsTextView
+        private val lockImageView = itemView.lockImageView
 
-        fun bind(locationGroup: String) {
+        fun bind(project: Project) {
             if (screen != Screen.PROFILE.id) {
                 checkImageView.visibility =
-                    if (locationGroup == selectedGroup) View.VISIBLE else View.GONE
+                    if (project.name == selectedGroup) View.VISIBLE else View.GONE
             }
 
-            locationGroupTextView.text = locationGroup
+            locationGroupTextView.text = project.name ?: itemView.context.getString(R.string.none)
+
+            permissionsTextView.text = project.permissions
+            lockImageView.visibility =
+                if (project.isGuest()) View.VISIBLE else View.GONE
+            setClickable(itemView, project.isGuest())
+
+            if (project.isGuest()) {
+                locationGroupTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.text_secondary))
+                itemView.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.statusColor))
+            } else {
+                locationGroupTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.text_black))
+                itemView.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.backgroundColor))
+            }
+        }
+    }
+
+    fun setClickable(view: View?, clickable: Boolean) {
+        if (view != null) {
+            if (view is ViewGroup) {
+                val viewGroup = view
+                for (i in 0 until viewGroup.childCount) {
+                    setClickable(viewGroup.getChildAt(i), clickable)
+                }
+            }
+            view.isClickable = clickable
         }
     }
 }
