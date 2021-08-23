cd8f8a2b2c97630510b5b7acedf008303d2d5a02
wasuphon <wasuphonn59@email.nu.ac.th>
2021-08-23 18:26:35 +0700

    String tidy

--- app/src/main/java/org/rfcx/companion/view/profile/guardiansoftware/GuardianSoftwareActivity.kt
+++ app/src/main/java/org/rfcx/companion/view/profile/guardiansoftware/GuardianSoftwareActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.toolbar_default.*
 import org.rfcx.companion.R
 
 class GuardianSoftwareActivity: AppCompatActivity() {
@@ -11,6 +12,16 @@ class GuardianSoftwareActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guardian_software)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            title = getString(R.string.guardian_softwares)
+        }
     }
 
     companion object {
