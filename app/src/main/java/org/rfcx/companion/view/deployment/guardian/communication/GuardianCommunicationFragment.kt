c0e4b68c57dbe704c91ce788132ac5aa1d61b76e
tooseriuz <frongs@rfcx.org>
2022-07-05 06:29:19 -0300

    Add tembe off times

--- app/src/main/java/org/rfcx/companion/view/deployment/guardian/communication/GuardianCommunicationFragment.kt
+++ app/src/main/java/org/rfcx/companion/view/deployment/guardian/communication/GuardianCommunicationFragment.kt
@@ -28,7 +28,8 @@ class GuardianCommunicationFragment : Fragment(), View.OnClickListener {
     private var indoAutoTimeOff = listOf("00:00-01:20", "03:10-08:40", "11:30-13:15", "15:05-20:45", "23:30-23:59")
     private var malayAutoTimeOff = listOf("00:00-01:15", "03:15-05:00", "06:00-09:00", "11:00-11:25", "12:00-13:20", "15:15-17:00", "18:00-20:45", "23:00-23:20")
     private var brazilAutoTimeOff = listOf("00:30-01:30", "03:40-05:20", "06:20-08:40", "15:30-17:15", "18:10-21:15", "23:15-23:35")
-    private var supportedSites = listOf("agk3cpurb5wm", "0cyons771mvx", "j8lnmj69yp45")
+    private var tembeAutoTimeOff = listOf("00:30-01:45", "03:25-04:30", "05:20-05:50", "06:40-08:40", "10:50-11:20", "12:15-13:45", "15:20-16:30", "17:00-17:40", "18:40-20:30", "21:25-23:10")
+    private var supportedSites = listOf("agk3cpurb5wm", "0cyons771mvx", "j8lnmj69yp45", "ceyn7evv79sy")
     private var tempStartHourOff: String? = null
     private var tempEndHourOff: String? = null
     private var isSetFirstGuardianPlan = false
@@ -266,6 +267,10 @@ class GuardianCommunicationFragment : Fragment(), View.OnClickListener {
                 if (deploymentProtocol?.getCurrentProjectId() == "j8lnmj69yp45") {
                     GuardianSocketManager.sendSatOnlyPrefs(brazilAutoTimeOff.joinToString(","))
                 }
+
+                if (deploymentProtocol?.getCurrentProjectId() == "ceyn7evv79sy") {
+                    GuardianSocketManager.sendSatOnlyPrefs(tembeAutoTimeOff.joinToString(","))
+                }
             }
         }
     }
@@ -286,6 +291,12 @@ class GuardianCommunicationFragment : Fragment(), View.OnClickListener {
 
         if (deploymentProtocol?.getCurrentProjectId() == "j8lnmj69yp45") {
             brazilAutoTimeOff.forEach { time ->
+                addChip(time, false)
+            }
+        }
+
+        if (deploymentProtocol?.getCurrentProjectId() == "ceyn7evv79sy") {
+            tembeAutoTimeOff.forEach { time ->
                 addChip(time, false)
             }
         }
