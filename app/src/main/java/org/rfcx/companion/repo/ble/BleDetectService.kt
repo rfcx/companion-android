1789299e91ec30f0b7ab59c9b1d0212d7bf911ff
tooseriuz <frongs@rfcx.org>
2022-09-06 19:01:52 +0700

    Remove imports

--- app/src/main/java/org/rfcx/companion/repo/ble/BleDetectService.kt
+++ app/src/main/java/org/rfcx/companion/repo/ble/BleDetectService.kt
@@ -6,9 +6,6 @@ import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.os.Build
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import org.rfcx.companion.entity.songmeter.Advertisement
@@ -38,7 +35,6 @@ class BleDetectService(context: Context) {
             super.onScanResult(callbackType, result)
 
             if (result != null) {
-                Log.d("BLE", result.device.address)
                 if (result.device.address.split(":").joinToString("").substring(0,6) == "9C25BE") {
                     address = result.device.address
                     advertisementUtils.convertAdvertisementToObject(result.scanRecord!!.bytes)
