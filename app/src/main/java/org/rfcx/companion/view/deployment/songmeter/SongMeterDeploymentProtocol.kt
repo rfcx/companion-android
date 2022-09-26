01822e9b3ed78f7f201eb26eb0e025ee0f0ab67d
tooseriuz <frongs@rfcx.org>
2022-09-26 15:00:30 +0700

    lint fix

--- app/src/main/java/org/rfcx/companion/view/deployment/songmeter/SongMeterDeploymentProtocol.kt
+++ app/src/main/java/org/rfcx/companion/view/deployment/songmeter/SongMeterDeploymentProtocol.kt
@@ -1,7 +1,6 @@
 package org.rfcx.companion.view.deployment.songmeter
 
 import org.rfcx.companion.entity.guardian.Deployment
-import org.rfcx.companion.entity.songmeter.Advertisement
 import org.rfcx.companion.view.deployment.BaseDeploymentProtocol
 
 interface SongMeterDeploymentProtocol : BaseDeploymentProtocol {
